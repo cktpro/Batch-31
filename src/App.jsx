31c4bf08b2dbb7e74a11c68427f7264cdd6c7229
Cáp Kim Trầm <cktpro@MacBook-Pro-cua-Cap.local>
2023-07-04 21:16:01 +0700

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import {  useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./Component/Layout";
 import Main from "./Component/Main";
-import Button from "./Component/ButtonGroup/Button";
-import MinusIcon from "./Component/Icon/minus";
-import Plus from "./Component/Icon/plus";
-import ImageSlide from "./Component/imageSlide";
-import imageList from './Component/fakeData/image.json';
 import PlayList from "./Component/playList";
+import Form from "Component/Form";
+import ButtonList from "Component/ButtonGroup/ButtonList";
+import CardList from "Component/CardList";
 
 function App() {
-  const [count, setCount] = useState(10);
   return (
     <div className="App">
-      <Layout title="Đây là trang app nè">
+      <Layout>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/media" element={<PlayList />} />
+        {/* <Route path="/products" element={<PlayList />} /> */}
+        <Route path="/form" element={<Form />} />
+        <Route path="/math" element={<ButtonList />} />
+        <Route path="/products" element={<CardList />} />
+        {/* <Route path="dashboard" element={<Dashboard />} /> */}
+        {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+      </Layout>
+      {/* <Layout title="Đây là trang app nè">
         <PlayList/>
         <ImageSlide imageList={imageList}/>
         <div
@@ -62,9 +74,22 @@ function App() {
           />
         </div>
         <Main />
-      </Layout>
+      </Layout> */}
     </div>
   );
 }
-
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+    </div>
+  );
+}
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
 export default App;
---
+++ src/Component/ButtonGroup/ButtonList.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import Plus from "Component/Icon/plus";
+import Minus from "Component/Icon/minus";
+
+function ButtonList(props) {
+  const [count, setCount] = useState(10);
+  return (
+    <div class="d-flex justify-content-center">
+      <div className="my-3">
+      <Button
+        icon={<Plus />}
+        title="Cộng"
+        buttonClass="button_secondary"
+        iconClass="icon_secondary"
+        titleClass="title_secondary"
+        count={count}
+        onClick={() => setCount(count + 1)}
+      />
+      <Button
+        icon={<Minus />}
+        title="Trừ"
+        buttonClass="button_primary"
+        iconClass="icon_primary"
+        titleClass="title_primary"
+        count={count}
+        onClick={() => setCount(count - 1)}
+      />
+      <Button
+        icon={<i class="fa-solid fa-xmark fa-2xl"></i>}
+        title="Nhân đôi"
+        buttonClass="button_secondary"
+        iconClass="icon_secondary"
+        titleClass="title_secondary"
+        count={count}
+        onClick={() => setCount(count * 2)}
+      />
+      <Button
+        icon={<i class="fa-solid fa-divide fa-2xl"></i>}
+        title="Chia đôi"
+        buttonClass="button_primary"
+        iconClass="icon_primary"
+        titleClass="title_primary"
+        count={count}
+        onClick={() => setCount(count / 2)}
+      />
+      </div>
+    </div>
+  );
+}
+
+export default ButtonList;
