a0e205561693475bb4a9d6877a18f53563cc2557
Cáp Kim Trầm <cktpro@MacBook-Pro-cua-Cap.local>
2023-07-05 22:30:38 +0700

    FormAPI

--- src/App.jsx
+++ src/App.jsx
@@ -6,75 +6,12 @@ import PlayList from "./Component/playList";
 import Form from "Component/Form";
 import ButtonList from "Component/ButtonGroup/ButtonList";
 import CardList from "Component/CardList";
+import { memo } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-      <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/media" element={<PlayList />} />
-        {/* <Route path="/products" element={<PlayList />} /> */}
-        <Route path="/form" element={<Form />} />
-        <Route path="/math" element={<ButtonList />} />
-        <Route path="/products" element={<CardList />} />
-        {/* <Route path="dashboard" element={<Dashboard />} /> */}
-        {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-      </Layout>
-      {/* <Layout title="Đây là trang app nè">
-        <PlayList/>
-        <ImageSlide imageList={imageList}/>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            margin: "30px",
-          }}
-        >
-          <Button
-            icon={<Plus />}
-            title="Cộng"
-            buttonClass="button_secondary"
-            iconClass="icon_secondary"
-            titleClass="title_secondary"
-            count={count}
-            onClick={() => setCount(count + 1)}
-          />
-          <Button
-            icon={<MinusIcon />}
-            title="Trừ"
-            buttonClass="button_primary"
-            iconClass="icon_primary"
-            titleClass="title_primary"
-            count={count}
-            onClick={() => setCount(count - 1)}
-          />
-          <Button
-            icon={<i class="fa-solid fa-xmark fa-2xl"></i>}
-            title="Nhân đôi"
-            buttonClass="button_secondary"
-            iconClass="icon_secondary"
-            titleClass="title_secondary"
-            count={count}
-            onClick={()=> setCount(count * 2)}
-          />
-          <Button
-            icon={<i class="fa-solid fa-divide fa-2xl"></i>}
-            title="Chia đôi"
-            buttonClass="button_primary"
-            iconClass="icon_primary"
-            titleClass="title_primary"
-            count={count}
-            onClick={()=> setCount(count / 2)}
-          />
-        </div>
-        <Main />
-      </Layout> */}
+      <Form/>
     </div>
   );
 }
@@ -85,11 +22,4 @@ function NoMatch() {
     </div>
   );
 }
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-export default App;
+export default memo(App);
